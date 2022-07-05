dedc3107162e96d311040146cdc3d9a3c94362eb
Kusuma Sandi <wijayakusumasandi@gmail.com>
2022-07-05 21:30:09 +0800

    Feature: notify payment

--- router/paymentRtr.js
+++ router/paymentRtr.js
@@ -6,6 +6,6 @@ const paymentController = require("../controllers/paymentCnt");
 
 router.post("/v1/direct-payment", verifyToken, paymentController.directPayment);
 router.post("/v1/payment-status", verifyToken, paymentController.paymentStatus);
-router.post("/v1/payment-notify", verifyToken, paymentController.paymentNotify);
+router.post("/v1/payment-notify", paymentController.paymentNotify);
 
 module.exports = router
