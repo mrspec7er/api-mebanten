d23bba7879d1ac480a24f6d175bb05130e433c2e
Kusuma Sandi <wijayakusumasandi@gmail.com>
2022-07-10 09:26:17 +0800

    Feature: register user

--- controllers/authCnt.js
+++ controllers/authCnt.js
@@ -1,9 +1,34 @@
+const admin = require("../config/firebaseConfig");
 exports.index = async (req, res) => {
 
     const {user_id} = res.locals;
 
+    const user = await admin.auth().getUser(user_id);
 
     res.status(200).json({
-        data: user_id
+        data: user
     })
+}
+
+exports.register = async (req, res) => {
+
+    try {
+        const {user_id} = res.locals;
+        const {email, phoneNumber, displayName} = req.body;
+
+        const user = await admin.auth().updateUser(user_id, {
+            email,
+            phoneNumber,
+            displayName
+        })
+
+        res.status(200).json({
+            data: user
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        })
+    }
 }
--- router/authRtr.js
+++ router/authRtr.js
@@ -5,5 +5,6 @@ const verifyToken = require("../middleware/clientAuth");
 const authController = require("../controllers/authCnt");
 
 router.get("/v1/user", verifyToken, authController.index);
+router.post("/v1/register", verifyToken, authController.register);
 
 module.exports = router
