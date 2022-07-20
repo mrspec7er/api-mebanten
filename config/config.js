14733a62636c4ea9bce72e41bac68ed45428e1f4
Kusuma Sandi <wijayakusumasandi@gmail.com>
2022-07-20 13:05:46 +0800

    Fix: database and firebase config settings

--- config/config.js
+++ config/config.js
@@ -1,50 +1,40 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-
-  "development": {
-    "username": process.env.DATABASE_USERNAME,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME,
-    "host": process.env.DATABASE_HOST,
-    "dialect": process.env.DATABASE_DIALECT,
-    "port": process.env.DATABASE_PORT,
-    "dialectOptions": {
-      "ssl": {
-       "require": true, 
-        "rejectUnauthorized": false 
-      }
-    }
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+    port: process.env.DATABASE_PORT,
   },
-  "test": {
-    "username": process.env.DATABASE_USERNAME,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME,
-    "host": process.env.DATABASE_HOST,
-    "dialect": process.env.DATABASE_DIALECT,
-    "port": process.env.DATABASE_PORT,
-    "dialectOptions": {
-      "ssl": {
-       "require": true, 
-        "rejectUnauthorized": false 
-      }
-    }
+  test: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+    port: process.env.DATABASE_PORT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
-  "production": {
-    "username": process.env.DATABASE_USERNAME,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME,
-    "host": process.env.DATABASE_HOST,
-    "dialect": process.env.DATABASE_DIALECT,
-    "port": process.env.DATABASE_PORT,
-    "dialectOptions": {
-      "ssl": {
-       "require": true, 
-        "rejectUnauthorized": false 
-      }
-    }
-  }
-}
-
-
-
+  production: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+    port: process.env.DATABASE_PORT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
