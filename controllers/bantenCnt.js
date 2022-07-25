8e3ada4e9b150a00197daacb9fa16a3ed50fe98d
Kusuma Sandi <wijayakusumasandi@gmail.com>
2022-07-26 07:55:52 +0800

    Feature: email verification

--- controllers/bantenCnt.js
+++ controllers/bantenCnt.js
@@ -1,233 +1,230 @@
-const {Banten_Upacara, Banten, Banten_Options, Banten_Option_Upacara, Griya,  Shipping, Address} = require("../models");
-const {Op, QuertTypes} = require("sequelize");
+const {
+  Banten_Upacara,
+  Banten,
+  Banten_Options,
+  Banten_Option_Upacara,
+  Griya,
+  Shipping,
+  Address,
+} = require("../models");
+const { Op, QuertTypes } = require("sequelize");
 
 exports.getAll = async (req, res) => {
+  const { name, offset, limit } = req.query;
 
-    const {name, offset, limit} =  req.query;
+  const { griya_id } = req.params;
 
-    const {griya_id} = req.params;
+  const banten = await Banten_Upacara.findAndCountAll({
+    offset,
+    limit,
+    where: {
+      griya_id,
+      name: {
+        [Op.iLike]: `%${name}%`,
+      },
+    },
+    include: [Griya],
+  });
 
-    const banten = await Banten_Upacara.findAndCountAll({
-        offset,
-        limit,
-        where: {
-            griya_id,
-            name:{
-                [Op.iLike]: `%${name}%`
-            }
-        
-        },
-        include: [Griya]
-    });
-
-    res.status(200).json({
-        data: banten
-    })
-}
+  res.status(200).json({
+    data: banten,
+  });
+};
 
 exports.getOne = async (req, res) => {
+  const { banten_id } = req.params;
 
-    const {banten_id} =  req.params
+  const banten = await Banten_Upacara.findOne({
+    where: {
+      id: banten_id,
+    },
+    include: [Griya],
+  });
 
-    const banten = await Banten_Upacara.findOne({
-        where: {
-            id: banten_id
-        },
-        include: [Griya]
-    });
-
-    res.status(200).json({
-        data: banten
-    })
-}
+  res.status(200).json({
+    data: banten,
+  });
+};
 
 exports.store = async (req, res) => {
+  const { name, price, min_order_day, desc, img, griya_id } = req.body;
 
-    const {name, price, min_order_day, desc, img, griya_id} = req.body
-    
-    const banten = await Banten_Upacara.create({
-        name,
-        price,
-        min_order_day,
-        desc,
-        img,
-        griya_id
-    });
+  const banten = await Banten_Upacara.create({
+    name,
+    price,
+    min_order_day,
+    desc,
+    img,
+    griya_id,
+  });
 
-    res.status(200).json({
-        data: banten
-    })
-}
+  res.status(200).json({
+    data: banten,
+  });
+};
 
 exports.storeOption = async (req, res) => {
+  const { name, price, banten_id } = req.body;
 
-    const {name, price, banten_id} = req.body;
-    
-    const bantenOptions = await Banten_Option_Upacara.create({
-        name,
-        price,
-        banten_id
-    });
+  const bantenOptions = await Banten_Option_Upacara.create({
+    name,
+    price,
+    banten_id,
+  });
 
-    res.status(200).json({
-        data: bantenOptions
-    })
-}
+  res.status(200).json({
+    data: bantenOptions,
+  });
+};
 
 exports.getOptions = async (req, res) => {
+  const { banten_id } = req.params;
 
-    const {banten_id} =  req.params;
+  const bantenOptions = await Banten_Option_Upacara.findAll({
+    where: {
+      banten_id,
+    },
+  });
 
-    const bantenOptions = await Banten_Option_Upacara.findAll({
-        where: {
-            banten_id
-        }
-    });
-
-    res.status(200).json({
-        data: bantenOptions
-    })
-}
-
+  res.status(200).json({
+    data: bantenOptions,
+  });
+};
 
 // Delivery Controller
 
 exports.deliveryGetAll = async (req, res) => {
+  const { name, offset, limit } = req.query;
+  const { address_id } = req.params;
+  const { user_id } = res.locals;
 
-    const {name, offset, limit} =  req.query;
-    const {address_id} = req.params;
-    const {user_id} = res.locals;
+  const address = await Address.findOne({
+    where: {
+      id: address_id,
+      user_id,
+    },
+  });
 
-    const address = await Address.findOne({
-        where: {
-            id: address_id,
-            user_id
-        }
+  if (!address) {
+    res.status(405).json({
+      error: "User address undefine",
     });
-
-    if (!address) {
-        res.status(405).json({
-            error: "User address undefine"
-        })
-    }
-
+  } else {
     const district_id = address.district_id;
 
     const banten = await Banten.findAndCountAll({
-        offset,
-        limit,
-        where: {
-            name:{
-                [Op.iLike]: `%${name}%`
-            }
-        
+      offset,
+      limit,
+      where: {
+        name: {
+          [Op.iLike]: `%${name}%`,
         },
-        include: [{
-            model: Shipping,
-            where: {
-                // griya_id: {[Op.col]: 'Griya_Banten.griya_id'},
-                // banten_id: {[Op.col]: 'Griya_Banten.banten_id'},
-                district_id
-            }
-        }, 
+      },
+      include: [
         {
-            model: Griya,
-        }
-    ]
-    })
+          model: Shipping,
+          where: {
+            // griya_id: {[Op.col]: 'Griya_Banten.griya_id'},
+            // banten_id: {[Op.col]: 'Griya_Banten.banten_id'},
+            district_id,
+          },
+        },
+        {
+          model: Griya,
+        },
+      ],
+    });
 
     res.status(200).json({
-        data: banten
-    })
-}
+      data: banten,
+    });
+  }
+};
 
 exports.deliveryGetOne = async (req, res) => {
+  const { address_id, banten_id } = req.params;
+  const { user_id } = res.locals;
 
-    const {address_id, banten_id} = req.params;
-    const {user_id} = res.locals;
+  const address = await Address.findOne({
+    where: {
+      id: address_id,
+      user_id,
+    },
+  });
 
-    const address = await Address.findOne({
-        where: {
-            id: address_id,
-            user_id
-        }
+  if (!address) {
+    res.status(405).json({
+      error: "User address undefine",
     });
+  }
 
-    if (!address) {
-        res.status(405).json({
-            error: "User address undefine"
-        })
-    }
+  const district_id = address.district_id;
 
-    const district_id = address.district_id;
-
-    const banten = await Banten.findOne({
+  const banten = await Banten.findOne({
+    where: {
+      id: banten_id,
+    },
+    include: [
+      {
+        model: Shipping,
         where: {
-            id: banten_id
+          // griya_id: {[Op.col]: 'Griya_Banten.griya_id'},
+          // banten_id: {[Op.col]: 'Griya_Banten.banten_id'},
+          district_id,
         },
-        include: [{
-            model: Shipping,
-            where: {
-                // griya_id: {[Op.col]: 'Griya_Banten.griya_id'},
-                // banten_id: {[Op.col]: 'Griya_Banten.banten_id'},
-                district_id
-            }
-        }, 
-        {
-            model: Griya
-        }
-    ]
-    })
+      },
+      {
+        model: Griya,
+      },
+    ],
+  });
 
-    res.status(200).json({
-        data: banten
-    })
-}
+  res.status(200).json({
+    data: banten,
+  });
+};
 
 exports.deliveryStore = async (req, res) => {
+  const { name, price, min_order_day, desc, img, griya_id } = req.body;
 
-    const {name, price, min_order_day, desc, img, griya_id} = req.body
-    
-    const banten = await Banten.create({
-        name,
-        price,
-        min_order_day,
-        desc,
-        img,
-        griya_id
-    });
+  const banten = await Banten.create({
+    name,
+    price,
+    min_order_day,
+    desc,
+    img,
+    griya_id,
+  });
 
-    res.status(200).json({
-        data: banten
-    })
-}
+  res.status(200).json({
+    data: banten,
+  });
+};
 
 exports.deliveryStoreOption = async (req, res) => {
+  const { name, price, banten_id } = req.body;
 
-    const {name, price, banten_id} = req.body;
-    
-    const bantenOptions = await Banten_Options.create({
-        name,
-        price,
-        banten_id
-    });
+  const bantenOptions = await Banten_Options.create({
+    name,
+    price,
+    banten_id,
+  });
 
-    res.status(200).json({
-        data: bantenOptions
-    })
-}
+  res.status(200).json({
+    data: bantenOptions,
+  });
+};
 
 exports.deliveryGetOptions = async (req, res) => {
+  const { banten_id } = req.params;
 
-    const {banten_id} =  req.params
+  const bantenOptions = await Banten_Options.findAll({
+    where: {
+      banten_id,
+    },
+  });
 
-    const bantenOptions = await Banten_Options.findAll({
-        where: {
-            banten_id
-        }
-    });
-
-    res.status(200).json({
-        data: bantenOptions
-    })
-}
+  res.status(200).json({
+    data: bantenOptions,
+  });
+};
--- middleware/clientAuth.js
+++ middleware/clientAuth.js
@@ -1,23 +1,24 @@
 const admin = require("../config/firebaseConfig");
 
 const verifyToken = async (req, res, next) => {
-    try {
-        const rawToken = req.headers.authorization;
-        const accessToken = rawToken.split(" ")[1];
-        const userData = await admin.auth().verifyIdToken(accessToken);
-        if (userData) {
-            res.locals.user_id = userData.user_id;
-            return next();
-        }
+  try {
+    const rawToken = req.headers.authorization;
+    const accessToken = rawToken.split(" ")[1];
+    const userData = await admin.auth().verifyIdToken(accessToken);
 
-        res.status(403).json({
-            error: "Unauthorize user"
-        });
-    } catch (err) {
-        res.status(500).json({
-            error: err.message
-        })
+    if (userData && userData.email_verified === true) {
+      res.locals.user_id = userData.user_id;
+      return next();
     }
-}
 
-module.exports = verifyToken
+    res.status(403).json({
+      error: "Unauthorize user",
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+};
+
+module.exports = verifyToken;
